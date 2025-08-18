6e04bbb228ede1108d6362f65c643be41601dbf1
Dishank-Sen <dishanksen05@gmail.com>
2025-08-19 00:48:41 +0530

    change

--- frontend/src/components/Test.jsx
+++ frontend/src/components/Test.jsx
@@ -1,5 +1,5 @@
 export default async function Test(){
-    const res = await fetch(`http://${import.meta.env.VITE_EC2_ENDPOINT}/test`)
+    const res = await fetch(`https://65.1.248.181/test`)
     console.log(res)
     return(
         <p>{res}</p>
